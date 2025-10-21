bc92c40489d56a42d13c861f25bc06637f3485bb
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-10-21 14:58:30 +0900

    スコアマネージャーの追加

---
+++ GameTemplate/Game/ScoreManager.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "ScoreManager.h"
+
+ScoreManager::ScoreManager()
+{
+}
+
+ScoreManager::~ScoreManager()
+{
+}
---
+++ GameTemplate/Game/ScoreManager.h
@@ -0,0 +1,24 @@
+#pragma once
+class ScoreManager:public IGameObject
+{
+public:
+	ScoreManager();
+	~ScoreManager();
+
+
+public:
+	void AddScore(float score) 
+	{
+		m_score = score;
+	};
+
+	float GetScore() {
+		return m_score;
+	}
+
+private:
+	float m_score = 0;
+
+
+};
+
