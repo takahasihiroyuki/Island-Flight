e0c2e70528800e8bcbe611e862b9677e6ca8e67d
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-12-24 18:38:27 +0900

    デバッグ用のシーン追加（途中）

--- GameTemplate/Game/FlightDebugScene.cpp
+++ GameTemplate/Game/FlightDebugScene.cpp
@@ -1,2 +1,19 @@
 #include "stdafx.h"
 #include "FlightDebugScene.h"
+
+FlightDebugScene::~FlightDebugScene()
+{
+}
+
+bool FlightDebugScene::Start()
+{
+	return false;
+}
+
+void FlightDebugScene::Update()
+{
+}
+
+void FlightDebugScene::Render(RenderContext& rc)
+{
+}
--- GameTemplate/Game/FlightDebugScene.h
+++ GameTemplate/Game/FlightDebugScene.h
@@ -3,7 +3,11 @@
 
 class FlightDebugScene : public IScene
 {
-
+	FlightDebugScene();
+	~FlightDebugScene();
+	bool Start() override;
+	void Update() override;
+	void Render(RenderContext& rc) override;
 
 	Aircraft* m_aircraft = nullptr;
 	PhysicsStaticObject m_ground;
